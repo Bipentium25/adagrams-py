8d269bf1668c9f59ab42f50758cba62f0525c8da
DayDayUp <bipentium25@gmail.com>
2025-09-11 16:54:00 -0700

    11.09.2025 mostly works details on wave 4 display need to rework

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,13 +1,69 @@
+import random
 from random import randint
 
 def draw_letters():
-    pass
+    pool = []
+    letter_counts = {
+    "A": 9, "B": 2, "C": 2, "D": 4, "E": 12, "F": 2,
+    "G": 3, "H": 2, "I": 9, "J": 1, "K": 1, "L": 4,
+    "M": 2, "N": 6, "O": 8, "P": 2, "Q": 1, "R": 6,
+    "S": 4, "T": 6, "U": 4, "V": 2, "W": 2, "X": 1,
+    "Y": 2, "Z": 1
+}
+  
+    for key, value in letter_counts.items():
+        for i in range(value+1):
+            pool.append(key)
+
+    pool_size = len(pool)
+    hand_cards = []
+    for i in range(10):
+        rand_index = random.randint(0,pool_size-1)
+        hand_cards.append(pool[rand_index])
+        pool.pop(rand_index)
+        pool_size -= 1
+    return hand_cards
 
 def uses_available_letters(word, letter_bank):
-    pass
+    if len(word) > 10:
+        return False
+    for letter in word:
+        if letter not in letter_bank:
+            return False
+        else:
+            letter_bank.remove(letter)
+    return True
 
 def score_word(word):
-    pass
-
+    word_point = 0
+    if len(word) > 6:
+        word_point += 8
+    letter_score = {
+    ('A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'): 1,
+    ('D', 'G'): 2,
+    ('B', 'C', 'M', 'P'): 3,
+    ('F', 'H', 'V', 'W', 'Y'): 4,
+    ('K',): 5,
+    ('J', 'X'): 8,
+    ('Q', 'Z'): 10    
+    }
+    for letter in word:
+        for key , value in letter_score.items(): 
+            if letter in key:
+                word_point += value
+    return word_point
+    
 def get_highest_word_score(word_list):
-    pass
+    winner_max = [word_list[0], score_word(word_list[0])]
+    for i in range(1, len(word_list)):
+        current_score = score_word(word_list[i])
+        print(word_list[i],current_score,winner_max)
+        ### cases where update needed
+        if current_score > winner_max[1]:
+            winner_max = [word_list[i], current_score]
+        elif current_score == winner_max and len(word_list[i]<len(winner_max[0])):
+            winner_max[0] = word_list[i]
+    
+    return winner_max[0],winner_max[1]
+
+
